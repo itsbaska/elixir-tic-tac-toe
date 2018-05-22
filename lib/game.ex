8cb4581fb1b6f2ff1e7a36df001fb06e8ad23e07
Baska Batbold <baskabatbold@gmail.com>
2018-05-22 09:06:59 -0500

    test the game struct

--- lib/game.ex
+++ lib/game.ex
@@ -1,6 +1,4 @@
 defmodule Game do
-  # import Player.mar
-  # use Board
   defstruct player_1: "X", player_2: "O", current_player: %Player{mark: "X"}, winner: nil, board: %Board{}
   
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -2,5 +2,7 @@ defmodule GameTest do
   use ExUnit.Case
   doctest Game
 
-  
+  test "test struct" do 
+    assert %Game{} = %Game{player_1: "X", player_2: "O", current_player: %Player{mark: "X"}, winner: nil, board: %Board{}}
+  end
 end
