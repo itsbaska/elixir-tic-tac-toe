62b39e199435ac729469d70be7e96ca447b33b4f
Baska Batbold <baskabatbold@gmail.com>
2018-05-23 12:50:42 -0500

    refactor validator

--- lib/validator.ex
+++ lib/validator.ex
@@ -1,12 +1,6 @@
 defmodule Validator do
-  def is_valid_input("0"), do: true  
-  def is_valid_input("1"), do: true
-  def is_valid_input("2"), do: true
-  def is_valid_input("3"), do: true
-  def is_valid_input("4"), do: true
-  def is_valid_input("5"), do: true
-  def is_valid_input("6"), do: true
-  def is_valid_input("7"), do: true
-  def is_valid_input("8"), do: true
-  def is_valid_input(_input), do: false
+
+  def is_valid_input("9"), do: false
+  def is_valid_input(input), do: Regex.match?(~r/\d/, input)
+
 end
