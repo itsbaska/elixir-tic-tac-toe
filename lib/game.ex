7d75ba88945dcc71fbf94d7751be2e858ebfef3f
Baska Batbold <baskabatbold@gmail.com>
2018-05-23 16:22:04 -0500

    check if board has 3 of the same marks in a row

--- lib/game.ex
+++ lib/game.ex
@@ -1,4 +1,29 @@
 defmodule Game do
-  defstruct player_1: %Player{}, player_2: %Computer{}, current_player: %Player{mark: "X"}, winner: nil, board: %Board{}
+  defstruct player_1: %Player{}, player_2: %Computer{}, current_player: %Player{mark: "X"}, winner: nil, board: %Board{}.spaces
   
+  def check_three([x, x, x]), do: true
+  def check_three([a, b, c]), do: false
+  
+  def check_line([head | tail]), do: [check_three(head) | check_line(tail)]
+  def check_line([]), do: []
+
+  def convert_to_columns([a, b, c, d, e, f, g, h, i]), do: [[a, d, g], [b, e, h], [c, f, i]]
+  def convert_to_diagonals([a, _b, c, _d, e, _f, g, _h, i]), do: [[a, e, i], [g, e, c]]
+  def convert_to_rows(board), do: Enum.chunk(board, 3)
+
+  def game_over?(board) do
+    cond do
+      Enum.any?(convert_to_rows(board) |> check_line, fn(x) -> x end) ->
+        true
+      Enum.any?(convert_to_columns(board) |> check_line, fn(x) -> x end) ->
+        true
+      Enum.any?(convert_to_diagonals(board) |> check_line, fn(x) -> x end) ->
+        true
+      Board.available_spaces(board) |> length == 0 ->
+        true
+      true ->
+        false
+    end
+  end
+
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -2,7 +2,65 @@ defmodule GameTest do
   use ExUnit.Case
   doctest Game
 
-  test "test struct" do 
-    assert %Game{} === %Game{player_1: %Player{mark: "X"}, player_2: %Computer{mark: "O"}, current_player: %Player{mark: "X"}, winner: nil, board: %Board{}}
+  test "struct" do 
+    assert %Game{} == %Game{player_1: %Player{mark: "X"}, player_2: %Computer{mark: "O"}, current_player: %Player{mark: "X"}, winner: nil, board: [0, 1, 2, 3, 4, 5, 6, 7, 8]}
   end
+
+  describe "when game is not over" do
+    test "board is empty" do
+      assert Game.game_over?([0, 1, 2, 3, 4, 5, 6, 7, 8]) == false
+    end
+
+    test "some spaces are filled" do
+      assert Game.game_over?(["X", 1, "O",
+                              3, "X", 5,
+                              6, 7, 8]) == false
+    end
+  end
+
+  describe "when game is over" do
+    test "first row win" do
+      assert Game.game_over?(["X", "X", "X",
+                              "O", "O", "X",
+                              "X", "O", "O"]) == true
+    end
+
+    test "sencond row win" do
+      assert Game.game_over?(["O", "X", "O",
+                              "X", "X", "X",
+                              "X", "O", "X"]) == true
+    end
+    
+    test "third row win" do
+      assert Game.game_over?(["X", "O", "O", 
+                              "O", "O", "X", 
+                              "X", "X", "X"]) == true
+    end
+
+    test "first column win" do
+      assert Game.game_over?(["X", "X", "O", 
+                              "X", "O", "X",
+                              "X", "O", "X"]) == true
+    end
+
+    test "sencond column win" do
+      assert Game.game_over?(["O", "X", "O", 
+                              "X", "X", "X",
+                              "O", "X", "X"]) == true
+    end
+    
+    test "third column win" do
+      assert Game.game_over?(["O", "X", "X",
+                              "O", "O", "X",
+                              "X", "O", "X"]) == true
+                              
+    end
+
+    test "tie" do
+      assert Game.game_over?(["X", "X", "O",
+                              "O", "O", "X",
+                              "X", "O", "X"]) == true
+    end
+  end
+
 end
