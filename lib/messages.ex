14a5ada69571419a7da4695ed8efcf4c08c0b1dd
Baska Batbold <baskabatbold@gmail.com>
2018-05-23 16:53:01 -0500

    return win or tie messages

--- lib/messages.ex
+++ lib/messages.ex
@@ -1,20 +1,29 @@
 defmodule Message do
   defstruct welcome: """
-                    Tic Tac Toe
-                    -----------
-                    Instructions:
-                    Select the spot you want to mark by entering the corresponding number.
-                    The first to get three marks in a row will win the game.
+                Tic Tac Toe
+                -----------
+                Instructions:
+                Select the spot you want to mark by entering the corresponding number.
+                The first to get three marks in a row will win the game.
 
-                    You are player "X", please start the game.
-                    """,
+                You are player "X", please start the game.
+                """,
 
             invalid: """
-                    Please enter a number between 0-8.
-                    """,
+                Please enter a number between 0-8.
+                """,
 
             spot_taken: """
-                    Spot is taken.
-                    Please try again at a different spot.
-                    """
+                Spot is taken.
+                Please try again at a different spot.
+                """,
+
+            win: """
+                Computer Wins!!!
+                """,
+
+            tie: """
+                It's a Tie!!!
+                """
+
 end
