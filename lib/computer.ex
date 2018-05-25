396c3966f9ac1eeef1df8cc2a589e5247d673612
Baska Batbold <baskabatbold@gmail.com>
2018-05-25 10:36:48 -0500

    check lowest or highest score

--- lib/computer.ex
+++ lib/computer.ex
@@ -4,6 +4,45 @@ defmodule Computer do
   def move(board, space) do
     Board.make_mark(board, space, %Computer{}.mark)
   end
-  
-  
+
+  def score(board) do
+    cond do
+      Game.get_winner(board) == "O" ->
+        1
+      Game.get_winner(board) == "X" ->
+        -1
+      Game.is_tie?(board) ->
+        0
+    end
+  end
+
+  def minimax_score("O", best_score) do
+    best_score |> Enum.max_by(fn(x) -> elem(x, 1) end) |> elem(1)
+  end
+
+  def minimax_score("X", best_score) do
+    best_score |> Enum.min_by(fn(x) -> elem(x, 1) end) |> elem(1)
+  end
+
+
+# iex(7)> list |> Enum.min_by(fn(x) -> elem(x, 1) end)    
+# {:e, -1}
+# iex(8)> list |> Enum.max_by(fn(x) -> elem(x, 1) end)
+# {:d, 3}
+
+# returns the space
+# def best_move(best_score)
+#   best_score.max_by { |key, value| value }[0]
+# end
+
+# returns the score
+# def minimax_score(game, best_score)
+#   if game.current_player.marker == @marker
+#     best_score.max_by { |key, value| value }[1]
+#   else
+#     best_score.min_by { |key, value| value }[1]
+#   end
+# end
+
+
 end
--- test/computer_test.exs
+++ test/computer_test.exs
@@ -8,7 +8,53 @@ defmodule ComputerTest do
   end
 
   test "make move" do
-    assert Computer.move(["X", "O", "X", "X", "O", "O", "O", "X", 8], 8) == ["X", "O", "X", "X", "O", "O", "O", "X", "O"]
+    assert Computer.move(["X", "O", "X",
+                          "X", "O", "O",
+                          "O", "X", 8], 8) == ["X", "O", "X",
+                                               "X", "O", "O",
+                                               "O", "X", "O"]
   end
 
+  describe "when game is over" do
+    test "return 1 if Computer wins" do
+      assert Computer.score(["O", "O", "O",
+                             "X", "O", "X",
+                             "O", "X", "O"]) == 1
+    end
+  
+    test "return -1 if Human wins" do
+      assert Computer.score(["X", "X", "X",
+                             "X", "O", "X",
+                             "O", "X", "O"]) == -1
+    end
+  
+    test "return 0 if tie" do
+      assert Computer.score(["X", "X", "O",
+                             "O", "O", "X",
+                             "X", "O", "X"]) == 0
+    end
+  end
+
+  describe "when O is winning" do
+    test "return the highest score" do
+      best_score = [{0, 1}, {1, -1}, {2, 0}, {0, 0}]
+      
+      assert Computer.minimax_score("O", best_score) == 1
+    end
+  end
+
+  describe "when X is winning" do
+    test "return the lowest score" do
+      best_score = [{0, 1}, {1, -1}, {2, 0}, {0, 0}]
+      assert Computer.minimax_score("X", best_score) == -1
+    end
+  end
+  # describe "when game is not over" do
+  #   test "return 0 if tie" do
+  #     assert Computer.score(["X", "X", "O",
+  #                            "O", 4, 5,
+  #                            6, "O", "X"]) == false
+  #   end
+  # end
+ 
 end
