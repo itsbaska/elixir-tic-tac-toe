6a3bbb4a7114f671c7b4fcee06b43ae499552f5a
Baska Batbold <baskabatbold@gmail.com>
2018-06-01 14:42:12 -0500

    computer randomly picks a space to move to

--- lib/computer.ex
+++ lib/computer.ex
@@ -1,9 +1,12 @@
 defmodule Computer do
   defstruct mark: "O"
   
-  def move(board, space) do
-    Board.make_mark(board, space, %Computer{}.mark)
+  def move(board, _space) do
+    space = Enum.random (0..8)
+    if Board.is_available?(board, space) do
+      Board.make_mark(board, space, %Computer{}.mark)
+    else
+      move(board, space)
+    end
   end
-  
-  
 end
