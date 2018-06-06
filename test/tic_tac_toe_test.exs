bedb9d8843aa79c24740910780d467439cd5ee66
Baska Batbold <baskabatbold@gmail.com>
2018-06-06 14:03:02 -0500

    remove comment

--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -2,35 +2,9 @@ defmodule TictacToeTest do
   use ExUnit.Case
   import ExUnit.CaptureIO
   doctest TicTacToe
-  
 
   test "create a new game" do
     assert TicTacToe.new == %Game{}
   end
 
-  # test "starts the game" do
-  #   test_output = fn ->
-  #     Runner.start
-  #   end
-  #   assert capture_io(test_output) ==  """
-  #   Tic Tac Toe
-  #   -----------
-  #   Instructions:
-  #   Select the spot you want to mark by entering the corresponding number.
-  #   The first to get three marks in a row will win the game.
-
-  #   You are player "X", please start the game.
-    
-  #   --------------
-
-  #     0   1   2
-  #    ===+===+=== 
-  #     3   4   5
-  #    ===+===+=== 
-  #     6   7   8
-
-  #   --------------
-
-  #   """
-  # end
 end
