2a95a76b3b00e468bb79dcc44dc1264fd2916497
Baska Batbold <baskabatbold@gmail.com>
2018-06-19 12:43:51 -0500

    refactor to be more readable

--- test/validator_test.exs
+++ test/validator_test.exs
@@ -3,52 +3,51 @@ defmodule ValidatorTest do
 
   doctest Validator
 
-  describe "when the user is playign a 3x3 game" do
-    test "return true if input is not a letter" do
+  describe ".is_valid_input, when the user is playing a 3x3 game" do
+    test "user inputs correct input" do
       assert Validator.is_valid_input?("1", 3) == true
     end
 
-    test "return false if input is a letter" do
+    test "user inputs letter" do
       assert Validator.is_valid_input?("C", 3) == false
     end
 
-    test "return false if input is out of bounds" do
+    test "user inputs number that is out of bounds" do
       assert Validator.is_valid_input?("9", 3) == false
     end
   end
 
-  describe "when the user is playign a 4x4 game" do
-    test "return true if input is not a letter" do
+  describe ".is_valid_input, when the user is playign a 4x4 game" do
+    test "user inputs a valid entry" do
       assert Validator.is_valid_input?("11", 4) == true
     end
 
-    test "return false if input is a letter" do
+    test "user input is a letter" do
       assert Validator.is_valid_input?("C", 4) == false
     end
 
-    test "return false if input is out of bounds" do
+    test "user input is out of bounds" do
       assert Validator.is_valid_input?("22", 4) == false
     end
   end
   
-  describe "when a user inputs a non existing option" do
-    test "return false if not an option" do
+  describe ".is_valid_option?" do
+    test "user inputs a non existing option" do
       assert Validator.is_valid_option?("3") == false
     end
 
-    test "return true if valid option" do
+    test "user inputs a valid existing option" do
       assert Validator.is_valid_option?("2") == true
     end
   end
   
-  describe "when a user inputs a user mark" do
-    test "return false if 2 marks are same" do
+  describe ".is_already_used?" do
+    test "when both users enter the same marks" do
       assert Validator.is_already_used?("3", "3") == true
     end
 
-    test "return true if 2 marks are different" do
+    test "when user marks are different" do
       assert Validator.is_already_used?("2", "X") == false
     end
   end
-  
 end
