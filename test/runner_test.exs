b20414d1c9baebea593eca4154833b9bac55b402
Baska Batbold <baskabatbold@gmail.com>
2018-06-19 10:00:24 -0500

    test runner

--- test/runner_test.exs
+++ test/runner_test.exs
@@ -5,6 +5,14 @@ defmodule RunnerTest do
 
   doctest Runner
 
+  describe ".start" do
+    test "when the user picks option 1, the game board size is 3x3" do
+      defmodule FakeStart do
+        def start, do: true
+      end
+      assert Runner.start(FakeStart) == true
+    end
+  end
   # test "start the game" do
   #   test_output = fn ->
   #     Runner.start
