2962a3311e30eed3b0f498aaa2d26c9179d8f596
Baska Batbold <baskabatbold@gmail.com>
2018-07-03 10:02:31 -0500

    add tests for invalid user input

--- lib/human.ex
+++ lib/human.ex
@@ -2,22 +2,32 @@ defmodule Player.Human do
   defstruct Player.fields
   alias Game.Board, as: Board
 
-  def get_user_move(game, console \\ Console) do
+  def get_user_move(game, console \\ Console, configuration \\ Player.Human) do
     move = console.get_move
-    case Validator.is_valid_input?(move, game.size) do
-      true ->
-        space = move |> Integer.parse |> elem(0)
-        if Board.is_available?(game, space) do
-          space
-        else
-          %Message{}.spot_taken |> console.print
-          game |> get_user_move
-        end
-      false ->
-        if game.size == 3, do: %Message{}.invalid_number_3x3, else: %Message{}.invalid_number_4x4
-        |> console.print
-        game |> get_user_move
+    move
+    |> Validator.is_valid_input?(game.size)
+    |> configuration.set_user_move(move, game, console)
+  end
+
+  def set_user_move(valid?, move, game, on_invalid_input \\ &get_user_move/2, console \\ Console)
+
+  def set_user_move(true, move, game, on_invalid_input, console) do
+    space = move |> Integer.parse |> elem(0)
+    if Board.is_available?(game, space) do
+      space
+    else
+      %Message{}.spot_taken 
+      |> console.print
+      game 
+      |> on_invalid_input.(game, console)
     end
+  end
+
+  def set_user_move(false, _move, game, on_invalid_input, console) do
+    if game.size == 3, do: %Message{}.invalid_number_3x3, else: %Message{}.invalid_number_4x4
+    |> console.print
+    game 
+    |> on_invalid_input.(game, console)
   end
 end
 
--- test/human_test.exs
+++ test/human_test.exs
@@ -23,7 +23,6 @@ defmodule HumanTest do
      assert Player.Move.move(game_1.current_player, game_1, PickMove) == 6
   end
 
-
   describe ".get_user_move" do
     test "when user move is 4" do
       defmodule PickMove3 do
@@ -36,6 +35,68 @@ defmodule HumanTest do
                   current_player: %Human{mark: "X"},
                   size: 3}
       assert Human.get_user_move(game, PickMove3) == 4
+    end
+
+    test "when user move is invalid" do
+      defmodule PickMove7 do
+        def get_move, do: "9"
+        def print(_message), do: nil
+      end
+      defmodule InvalidPick3 do
+        def set_user_move(_, _, _, _), do: :was_called
+      end
+      game = %Game{board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                  player_1: %Human{mark: "X"},
+                  player_2: %Human{mark: "O"},
+                  current_player: %Human{mark: "X"},
+                  size: 3}
+      assert Human.get_user_move(game, PickMove7, InvalidPick3) == :was_called
+    end
+  end
+
+  describe ".set_user_move" do
+    test "when user move is 4" do
+      defmodule PickMove4 do
+        def print(_message), do: nil
+      end
+      game = %Game{board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                  player_1: %Human{mark: "X"},
+                  player_2: %Human{mark: "O"},
+                  current_player: %Human{mark: "X"},
+                  size: 3}
+      assert Human.set_user_move(true, "4", game, PickMove4) == 4
+    end
+
+    test "when user move is 9" do
+      defmodule PickMove5 do
+        def print(_message), do: nil
+      end
+
+      defmodule InvalidPick do
+        def test_call(_, _, _), do: :was_called
+      end
+      game = %Game{board: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                  player_1: %Human{mark: "X"},
+                  player_2: %Human{mark: "O"},
+                  current_player: %Human{mark: "X"},
+                  size: 3}
+      assert Human.set_user_move(false, "4", game, &InvalidPick.test_call/3, PickMove5) == :was_called
+    end
+
+    test "when user move is 4 and spot is taken" do
+      defmodule PickMove6 do
+        def print(_message), do: nil
+      end
+
+      defmodule InvalidPick2 do
+        def test_call(_, _, _), do: :was_called
+      end
+      game = %Game{board: [0, 1, 2, 3, "X", 5, 6, 7, 8],
+                  player_1: %Human{mark: "X"},
+                  player_2: %Human{mark: "O"},
+                  current_player: %Human{mark: "X"},
+                  size: 3}
+      assert Human.set_user_move(true, "4", game, &InvalidPick2.test_call/3, PickMove6) == :was_called
     end
   end
 end
